e2501451d747b2ac3ffd6320552b301fdae29b2b
TobbyFranc <tobbyfranc@gmail.com>
2022-06-23 22:29:34 +0100

    adjusted

---
+++ lay.js
@@ -0,0 +1,8 @@
+let Fname = document.getElementsByClassName("")
+let Lname = document.getElementsByClassName("")
+let Umail = document.getElementsByClassName("")
+let password = document.getElementsByClassName("")
+
+Fname.onfill = function (){
+    
+}
